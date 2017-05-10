c7da2d1f9cc1296a67db31a54c69ec1f09875f65
Alejandro E. Losada <alosada@gmail.com>
2017-05-09 22:54:36 -0500

    readme modifications

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Game, :type => :model do
   it "checks that the score " do
     # game = build(:game)
     game = Game.create!(date_of_game: Date.today, score: {winner: 21, looser: 15}.to_json, winner: create(:user))
-    game.winner = build(:user)
+    user = build(:user)
+    game.winner = user
+    game.users << user
+    game.users << build(:user)
     expect(game.save).to be_true
     # game = build(:game_bad_score)
     game = Game.create!(date_of_game: Date.today, score: {winner: 21, looser: 20}.to_json, winner: create(:user))
